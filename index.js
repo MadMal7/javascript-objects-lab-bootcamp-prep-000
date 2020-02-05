ddf61a668d0a65c9328084c27bbc542d02fba819
Malachi Scott Prah <malachiprah@gmail.com>
2020-02-05 20:38:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-var recipies = {};
+var recipie = {}
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]: value })
-}
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
   return object
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete newObj[key]
-  return newObj
+  return Object.assign({}, object[key] )
 }
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
   return object
 }
