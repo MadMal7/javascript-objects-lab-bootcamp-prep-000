445eaab2b00beccc31837be567a5b66677c77080
Malachi Scott Prah <malachiprah@gmail.com>
2020-02-05 20:40:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object[key]: value )
+}
